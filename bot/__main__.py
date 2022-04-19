746277d2232471a416959a8ddb6af806e95d10a6
codexisnothere <88002839+codexisnothere@users.noreply.github.com>
2022-04-19 23:43:15 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,7 +3,7 @@ import time
 from telegram.ext import CommandHandler
 
 from bot import AUTHORIZED_CHATS, dispatcher, updater
-from bot.modules import auth, clone, count, delete, list, permission, shell
+from bot.modules import auth, clone, count, delete, list, permission, shell, bypass
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import *
